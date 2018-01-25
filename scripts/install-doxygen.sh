4b7b3408bcf64d1e5f6c160983cff970c1c4e09a
thephez <thephez@users.noreply.github.com>
2018-01-25 14:43:57 -0500

    build - Quiet wget and execute doxygen install first

--- scripts/install-doxygen.sh
+++ scripts/install-doxygen.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -ex
-wget http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.14.linux.bin.tar.gz
+wget -q http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.14.linux.bin.tar.gz
+
+echo "Decompressing..."
 tar -xzvf doxygen-1.8.14.linux.bin.tar.gz
+
+echo "Configure and Install Doxygen"
 cd doxygen-1.8.14 && ./configure && make install
