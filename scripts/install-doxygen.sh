3b9279c2119fe3cbd75e18b6d6d1126cccfa1e76
thephez <thephez@users.noreply.github.com>
2018-01-25 15:45:12 -0500

    build - Try using doxygen binary directly

--- scripts/install-doxygen.sh
+++ scripts/install-doxygen.sh
@@ -5,5 +5,8 @@ wget -q http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.14.linux.bin.tar.gz
 echo "Decompressing..."
 tar -xzf doxygen-1.8.14.linux.bin.tar.gz
 
-echo "Configure and Install Doxygen"
-cd doxygen-1.8.14 && ./configure && sudo make install
+cp doxygen-1.8.14/bin/* ~
+ls ~
+
+#echo "Configure and Install Doxygen"
+#cd doxygen-1.8.14 && ./configure && sudo make install
