553c69a336756ecaf1c2dff52447f8e4ff720fea
thephez <thephez@users.noreply.github.com>
2018-01-25 14:54:09 -0500

    build - add retry for doxygen install and suppress tar output

--- scripts/install-doxygen.sh
+++ scripts/install-doxygen.sh
@@ -3,7 +3,7 @@ set -ex
 wget -q http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.14.linux.bin.tar.gz
 
 echo "Decompressing..."
-tar -xzvf doxygen-1.8.14.linux.bin.tar.gz
+tar -xzf doxygen-1.8.14.linux.bin.tar.gz
 
 echo "Configure and Install Doxygen"
 cd doxygen-1.8.14 && ./configure && make install
