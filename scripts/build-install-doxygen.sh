fc7f622ee5bf58e4a8cec484e96369d455f1a72a
thephez <thephez@users.noreply.github.com>
2018-02-01 15:41:57 -0500

    Doxygen build updates  - Add doxygen build/install script  - Update travis config and script to work with changes

---
+++ scripts/build-install-doxygen.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -ex
+
+# Build instructions from: https://www.stack.nl/~dimitri/doxygen/download.html
+echo "Cloning doxygen repository..."
+git clone https://github.com/doxygen/doxygen.git doxygen-src
+
+cd doxygen-src
+
+# Use v1.8.14 (2f4139de014bf03898320a45fe52c92872c1e0f4)
+git checkout 2f4139de014bf03898320a45fe52c92872c1e0f4 .
+
+echo "Create build folder..."
+mkdir build
+cd build
+
+echo "Make..."
+cmake -G "Unix Makefiles" ..
+make
+
+echo "Done building doxygen..."
+./bin/doxygen -v
+
+echo "doxygen path: " $(pwd)
+
+echo "Copy Doxygen binaries to $TRAVIS_BUILD_DIR"
+cp ./bin/* $TRAVIS_BUILD_DIR
+
+cd $TRAVIS_BUILD_DIR
