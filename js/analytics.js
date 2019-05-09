e56d26f91953b5b88e125e0d325207704fcf2324
thephez <thephez@users.noreply.github.com>
2019-05-09 13:27:17 -0400

    Config - Enable analytics IP anonymization

--- js/analytics.js
+++ js/analytics.js
@@ -5,6 +5,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-118991016-3', 'auto');
+  ga('set', 'anonymizeIp', true);
   ga('send', 'pageview');
 
 /* jshint ignore:end */
