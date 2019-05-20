85f779bfc5a8d903ae0ce9340be6281e937a98d4
thephez <thephez@users.noreply.github.com>
2019-05-20 12:08:22 -0400

    Config - analytics (#130)
    * Config - Update analytics
    
    * Config - Enable analytics IP anonymization

--- js/analytics.js
+++ js/analytics.js
@@ -4,7 +4,8 @@
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-46627233-1', 'auto');
+  ga('create', 'UA-118991016-3', 'auto');
+  ga('set', 'anonymizeIp', true);
   ga('send', 'pageview');
 
 /* jshint ignore:end */
